c0d39e4e46d36fc040f314b903ffe3da2e390aba
mrToad <korostylev.ov@gmail.com>
2022-05-16 23:33:41 +0300

    lesson5

--- 3/client.py
+++ 3/client.py
@@ -12,8 +12,13 @@ import json
 import socket
 import time
 import sys
+import logging
+from logs import client_log_config
 from common.utils import get_message, send_message
 from common.variables import DEFAULT_PORT, DEFAULT_IP_ADDR, ACTION, TIME, USER, ACCOUNT_NAME, PRESENCE, RESPONSE, ERROR
+
+# Создание именованного логгера для сервера
+client_logger = logging.getLogger('client_log')
 
 # инициализация пользователя
 def create_presence(account_name='Guest'):
@@ -24,10 +29,12 @@ def create_presence(account_name='Guest'):
             ACCOUNT_NAME: account_name
         }
     }
+    client_logger.debug(f'Создано сообщение {PRESENCE} для пользователя {account_name}')
     return out
 
 # обработка ответа сервера
 def process_ans(message):
+    client_logger.debug(f'Получен ответ от сервера: {message}')
     if RESPONSE in message:
         if message[RESPONSE] == 200:
             return '200 : OK'
@@ -45,11 +52,16 @@ def main():
     except IndexError:
         server_address = DEFAULT_IP_ADDR
         server_port = DEFAULT_PORT
+        client_logger.debug(f'Значение IP-адреса установлено по умолчанию: {DEFAULT_IP_ADDR}')
+        client_logger.debug(f'Значение порта установлено по умолчанию: {DEFAULT_PORT}')
     except ValueError:
-        print('Номер порта должен быть в диапазоне от 1024 до 65535')
+        # print('Номер порта должен быть в диапазоне от 1024 до 65535')
+        client_logger.critical(f'Номер порта {server_port} должен быть в диапазоне от 1024 до 65535')
         sys.exit(1)
-    print(server_address)
-    print(server_port)
+    # print(server_address)
+    # print(server_port)
+    client_logger.debug(f'Получен IP-адрес: {server_address}')
+    client_logger.debug(f'Получен порт: {server_port}')
 
     # Инициализация сокета и обмен данными
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -58,9 +70,11 @@ def main():
     send_message(transport, message_to_server)
     try:
         answer = process_ans(get_message(transport))
-        print(answer)
+        # print(answer)
+        client_logger.debug(f'Получено сообщение от сервера: {answer}')
     except (ValueError, json.JSONDecodeError):
-        print('Некорректное сообщение от сервера')
+        # print('Некорректное сообщение от сервера')
+        client_logger.error('Некорректное сообщение от сервера')
 
 if __name__ == '__main__':
     main()
---
+++ 3/logs/server_log_config.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import logging
+import logging.handlers
+
+server_formatter = logging.Formatter('%(asctime)-26s %(levelname)-10s %(filename)s %(message)s')
+'''
+где:
+%(asctime)s - Время, когда была выполнена запись в журнал, в формате ASCII
+%(levelname)s - Символическое имя уровня важности (DEBUG, INFO и т.д.)
+%(filename)s - Имя исходного файла, откуда была выполнена запись в журнал
+%(message)s - Текст журналируемого сообщения (определяется пользователем)
+'''
+
+sys.path.append('../')
+path = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(path, 'server_log.log')
+
+stream_handler = logging.StreamHandler(sys.stderr)
+stream_handler.setFormatter(server_formatter)
+stream_handler.setLevel(logging.DEBUG)
+log_file = logging.handlers.TimedRotatingFileHandler(path, encoding='utf-8', interval=1, when='midnight', utc=False)
+log_file.setFormatter(server_formatter)
+
+logger = logging.getLogger('server_log')
+logger.addHandler(stream_handler)
+logger.addHandler(log_file)
+logger.setLevel(logging.DEBUG)
+
+if __name__ == '__main__':
+    logger.debug('отладочная информация')
+    logger.info('отладочное сообщение')
+    logger.warning('предупреждение')
+    logger.error('ошибка')
+    logger.critical('критическая ошибка')
